e8e0ab69366cfb1363fc3d8ff3aded48f6dbb2ca
daviribeiro <luminuszz43@gmail.com>
2023-06-11 16:03:00 -0300

    protect methods

--- src/game/GameUi.java
+++ src/game/GameUi.java
@@ -3,25 +3,19 @@ package game;
 import java.util.Scanner;
 
 public abstract class GameUi {
-
     private final Scanner scanner;
 
 
     public GameUi() {
-
         this.scanner = new Scanner(System.in);
-
-
     }
 
 
-    public void showMessage(String message) {
+    protected void showMessage(String message) {
         System.out.println(message);
     }
 
-
-    public int getIntParam() {
-
+    protected int getIntParam() {
         return this.scanner.nextInt();
     }
 
